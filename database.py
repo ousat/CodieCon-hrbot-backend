a36afcd639a15ee6840e64bdc010cc26686f3f92
Satish Kumar <s.n.kumar.satish.sk@gmail.com>
2019-03-09 02:11:05 +0530

    file upload changed and database models for users and saved questions updated

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 import pymysql
 
 INSERT_BASIC_DETAILS = '''INSERT INTO Profile (identification_number, identification_type, name, email) VALUES ('{idno}', '{idty}', '{name}', '{email}');'''
+GET_DETAILS = '''select id from Profile where identification_number='{idno}' and identification_type='{idty}' and name='{name}' and email='{email}';'''
 
 def connect():
     return pymysql.connect(
@@ -12,14 +13,26 @@ def connect():
 
 def save_profile(idno, idty, name, email):
     try:
+        # print(idno)
         connection = connect()
         cursor = connection.cursor()
         insertStatement = INSERT_BASIC_DETAILS.format(idno=idno, idty=idty, name=name, email=email)
         # print(insertStatement)
         cursor.execute(insertStatement)
         connection.commit()
-        cursor.close()
+        select_statement = GET_DETAILS.format(idno=idno, idty=idty, name=name, email=email)
+        # print(select_statement)
+        cursor.execute(select_statement)
+        # print(cursor.fetchall())
+        value = cursor.fetchone()
         connection.close()
-        return True
+        return {"status": True, "user_id": value[0]}
+    except KeyError as ke:
+        print(ke)
+        return False
     except:
         return False
+
+
+def save_user_qsn_answer():
+    pass
--- server.py
+++ server.py
@@ -1,4 +1,7 @@
-from flask import Flask, jsonify, request, redirect, url_for
+import os
+
+from flask import Flask, jsonify, request, redirect, url_for, send_file
+from werkzeug import secure_filename
 from database import save_profile
 
 app = Flask(__name__)
@@ -17,8 +20,15 @@ def index():
     return jsonify({})
 
 @app.route('/rqsn')
-def hello():
+def rqsn():
     return jsonify({"question": "what are some of the most prominent features of Python?"})
+
+
+@app.route('/resume')
+def resume():
+    # with open('/home/user/codiecon/hrbot/Resume_1.pdf', 'rb') as static_file:
+    return send_file('/home/user/codiecon/hrbot/Resume_1.pdf')
+
 
 @app.route('/profile-details', methods=['POST'])
 def profile_form():
@@ -26,23 +36,19 @@ def profile_form():
     idty = request.form["id_type"]
     name = request.form["name"]
     email = request.form["email"]
-    status =  save_profile(idno, idty, name, email)
-    return jsonify({"status": status})
+    # print(idno)
+    status = save_profile(idno, idty, name, email)
+    if not type(status) == dict:
+        return jsonify({"status": False})
+    return jsonify(status)
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    uploaded_files = request.files.getlist("file[]")
-    filenames = []
-    for file in uploaded_files:
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            filenames.append(filename)
-            return jsonify({"upload": True, "filename": filename})
-        else:
-            return jsonify({"upload": False, "message": "invalid file"})
-
-    return jsonify({})
+    f = request.files['file']
+    print(f)
+    f.save(secure_filename(f.filename))
+    print(f.filename)
+    return jsonify({"filename": f.filename})
 
 
 if __name__ == "__main__":
